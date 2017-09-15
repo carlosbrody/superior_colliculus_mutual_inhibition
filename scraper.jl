d1e8365f95fcf56c4568164c26d017846f733393
Unknown <carlosbrody@gmail.com>
2017-09-15 01:26:16 -0400

    Tweaks on scraper system -- now multiple cells can go into one file

--- scraper.jl
+++ scraper.jl
@@ -1,5 +1,13 @@
+# DON'T MODIFY THIS FILE -- the source is in file Scraper.ipynb
+
 
 using JSON
+
+
+# DON'T MODIFY THIS FILE -- the source is in file Scraper.ipynb
+
+
+# Scrape a notebook for julia code that should be written into an indicated file
 
 """
 filenames_written = scrape_notebook(notebook_filename; verbose=false, includemagic="#@include_me")
@@ -8,25 +16,37 @@ Goes through a file notebook_filename, assuming it is an ipynb, and looks for co
 includemagic, followed by whitespace, followed by a string (which we shall call filename). 
 When such a code cell is found, its contents are written into filename.
 
+If more than one cell uses the same filename, then the first one starts the file, and subsequent cells
+append to it.
+
 Returns an array with the written filenames
 
 """
 function scrape_notebook(notebook_filename; verbose=false, includemagic="#@include_me")
 
-    filenames = [];    
+    filenames = [];    # List of output files found in this notebook
     A = JSON.parse(readstring(notebook_filename))
+    
     for mycell in A["cells"]
-        if mycell["cell_type"] == "code"
+        if mycell["cell_type"] == "code"   
             lines = mycell["source"]
-            if length(lines)>0
+            if length(lines)>0           # We only consider code cells that are not empty
                 m= match(Regex(@sprintf("(?<include>%s)\\s*(?<filename>\\S*)", includemagic)), lines[1])
-                if typeof(m)!=Void && length(m["filename"])>0
-                    if verbose; @printf("Writing out file %s\n", m["filename"]); end
-                    filenames = [filenames ; m["filename"]]
-                    f = open(m["filename"], "w")
+                if typeof(m)!=Void && length(m["filename"])>0  # proceed if we got a match and got a filename
+                    if any(filenames .== m["filename"]); 
+                        f = open(m["filename"], "a")           # we'll append if we already had that filename
+                        if verbose; @printf("Appending to file %s\n", m["filename"]); end
+                    else
+                        f = open(m["filename"], "w")           # otherwise open fresh for writing
+                        filenames = [filenames ; m["filename"]]
+                        if verbose; @printf("Writing out file %s\n", m["filename"]); end
+                    end
+                    # Now write out the contents of the cell, with a warning at the top:
+                    write(f, @sprintf("# DON'T MODIFY THIS FILE -- the source is in file %s\n\n", notebook_filename))
                     for i=2:length(lines)
                         write(f, lines[i])
                     end
+                    write(f, "\n\n\n")
                     close(f)
                 end
             end
@@ -37,6 +57,10 @@ function scrape_notebook(notebook_filename; verbose=false, includemagic="#@inclu
 end
 
 
+# DON'T MODIFY THIS FILE -- the source is in file Scraper.ipynb
+
+
+# Get database of scraped files
 
 """
 latest = latest_scrapedict(; scrapedir=".scrapedir", scrapefile="scrapelist")
@@ -56,7 +80,7 @@ function latest_scrapedict(; scrapedir=".scrapedir", scrapefile="scrapelist")
     try
         A = readdlm(sfile, ',')
         for i=1:size(A,1)
-            get!(answer, A[i,1], lstrip(A[i,2]))
+            get!(answer, lstrip(A[i,2]), A[i,1])
         end
     catch
         answer = Dict()
@@ -65,6 +89,29 @@ function latest_scrapedict(; scrapedir=".scrapedir", scrapefile="scrapelist")
 end
 
 
+"""
+write_scrapedict(latest; scrapedir=".scrapedir", scrapefile="scrapelist")
+
+Writes a dictionary containing filename-latest_scrape_time_string pairs into a file
+
+"""
+function write_scrapedict(latest; scrapedir=".scrapedir", scrapefile="scrapelist")
+
+    sfile = scrapedir * "/" * scrapefile
+    sf = open(sfile, "w")
+    for k in keys(latest)
+        write(sf, @sprintf("%s, %s\n", latest[k], k))
+    end
+    close(sf)
+
+end
+
+
+# DON'T MODIFY THIS FILE -- the source is in file Scraper.ipynb
+
+
+# Go through all notebooks in directory and scrape them if they've been modified after their last
+# scrape time.
 
 """
 rescraped = scrape_all_notebooks(; scrapedir=".scrapedir", scrapefile="scrapelist", verbose=false)
@@ -89,14 +136,24 @@ function scrape_all_notebooks(; scrapedir=".scrapedir", scrapefile="scrapelist",
         end
     end
 
-    sfile = scrapedir * "/" * scrapefile
-    sf = open(sfile, "w")
-    for k in keys(latest)
-        write(sf, @sprintf("%s, %s\n", k, latest[k]))
-    end
-    close(sf)
+    write_scrapedict(latest; scrapedir=scrapedir, scrapefile=scrapefile)
     
     return rescraped
 end
 
-scrape_all_notebooks()
+
+
+
+# DON'T MODIFY THIS FILE -- the source is in file Scraper.ipynb
+
+
+function scraperobot()
+    while true
+        scrape_all_notebooks()
+        sleep(2)
+    end
+end
+
+scraperobot()
+
+
