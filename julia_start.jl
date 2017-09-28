01d0b906d98439b2b36022e5049356615030726e
Unknown <carlosbrody@gmail.com>
2017-09-28 16:28:32 -0400

    Fixing install to use system-wide instead Julia-specific python
    For whatever reason, the Julia-specific python started choking on using matplotlib... but installing python and matplotlib externally, and pointing Julia to using that (ENV["PYTHON"] = "/usr/bin/python") seems to work fine.

--- julia_start.jl
+++ julia_start.jl
@@ -1,9 +1,10 @@
 # Run this file in julia, and it will add and build all the necessary packages for cost function minimization
 
-ENV["PYTHON"]=""
+ENV["PYTHON"]="/usr/bin/python"
 Pkg.add("PyCall")
 Pkg.build("PyCall")
 Pkg.add("PyPlot")
+Pkg.build("PyPlot")
 using PyPlot
 
 Pkg.add("ForwardDiff")
--- package_installs.sh
+++ package_installs.sh
@@ -9,19 +9,20 @@ sudo apt-get -y install build-essential
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install git-core
-git config --global user.email "marinopagan@gmail.com"
-git config --global user.name "Marino Pagan"
+git config --global user.email "carlosbrody@gmail.com"
+git config --global user.name "Carlos Brody"
 sudo apt-get -y install emacs
 sudo apt-get -y install python-dev
+sudo apt-get -y install python-matplotlib
 sudo apt-get -y install csh
+sudo apt-get -y install hdf5-tools
 
 wget https://julialang-s3.julialang.org/bin/linux/x64/0.5/julia-0.5.2-linux-x86_64.tar.gz
 tar xvfz julia-0.5.2-linux-x86_64.tar.gz 
-sudo ln -s /home/marinopagan/julia-f4c6c9d4bb/bin/julia /usr/bin/julia
+sudo ln -s /home/carlosbrody/julia-f4c6c9d4bb/bin/julia /usr/bin/julia
 
-sudo apt-get -y install hdf5-tools
 
-git clone "http://marinopagan@github.com/carlosbrody/superior_colliculus_mutual_inhibition.git"
+git clone "http://carlosbrody:gungaDID90&@github.com/carlosbrody/superior_colliculus_mutual_inhibition.git"
 
 # THEN START JULIA AND RUN THIS FILE:
 
