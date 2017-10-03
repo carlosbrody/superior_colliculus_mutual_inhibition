7c21a329a6f43c80d821dcda75780b023abd6475
Unknown <carlosbrody@gmail.com>
2017-10-03 18:48:05 -0400

    has been made compatible with both Julia 0.5.2 and Julia 0.6

--- hessian_utils.jl
+++ hessian_utils.jl
@@ -83,70 +83,160 @@ end
 
 using ForwardDiff
 
+"""
+v = FDversion()
 
-# """
-# We define functions to convert Duals, the variable types used by ForwardDiff, 
-# to Floats. This is useful if we want to print out the value of a variable 
-# (since print doesn't know how to Duals). Note that after being converted to a Float, no
-# differentiation by ForwardDiff can happen!  e.g. after
-#     x = convert(Float64, y)
-# ForwardDiff can still differentiate y, but it can't differentiate x
-# """
-
-import Base.convert
-convert(::Type{Float64}, x::ForwardDiff.Dual) = Float64(x.value)
-function convert(::Array{Float64}, x::Array{ForwardDiff.Dual}) 
-    y = zeros(size(x)); 
-    for i in 1:prod(size(x)) 
-        y[i] = convert(Float64, x[i]) 
+Return the installed version of ForwardDiff as a floating point, major.minor
+"""
+function FDversion()
+    try
+        return Pkg.installed("ForwardDiff").major + 0.1*Pkg.installed("ForwardDiff").minor
+    catch
+        error("Is ForwardDiff really installed???")
     end
-    return y
+end
+
+
+if FDversion() < 0.6
+    # --------------------------------------------------------------
+    #
+    #               FOR FORWARDDIFF < 0.6   (Julia 0.5.2)
+    #
+    # --------------------------------------------------------------
+    
+    
+    # """
+    # We define functions to convert Duals, the variable types used by ForwardDiff, 
+    # to Floats. This is useful if we want to print out the value of a variable 
+    # (since print doesn't know how to Duals). Note that after being converted to a Float, no
+    # differentiation by ForwardDiff can happen!  e.g. after
+    #     x = convert(Float64, y)
+    # ForwardDiff can still differentiate y, but it can't differentiate x
+    # """
+
+    import Base.convert
+    convert(::Type{Float64}, x::ForwardDiff.Dual) = Float64(x.value)
+    function convert(::Array{Float64}, x::Array{ForwardDiff.Dual}) 
+        y = zeros(size(x)); 
+        for i in 1:prod(size(x)) 
+            y[i] = convert(Float64, x[i]) 
+        end
+        return y
+    end
+    
+    
+
+    """
+    function M = ForwardDiffZeros(m, n; nderivs=0, difforder=0)
+
+    Use instead of zeros(). Creates a matrix of zeros, of size m rows by n columns, with elements appropriate for 
+    differentiation by ForwardDiff. If nderivs==0 or difforder==0 then the elements will be regular
+    Float64, not ForwardDiff types.
+
+    PARAMETERS:
+    ===========
+
+    m        Integer, number of rows
+
+    n        Integer, number of columns
+
+
+    OPTIONAL PARAMETERS:
+    ====================
+
+    nderivs=0       The number of variables that we'll be differentiating with respect to. In other
+                    words, this number is equal to the length of the gradient. If this is left as zero (the default) then 
+                    the data type will be regular Float64
+
+    difforder=0     The order of the derivative we will want to take.  Zero means nothing, stick with
+                    regular Float64, 1 means gradient, 2 means hessian
+
+    RETURNS:
+    ========
+
+    An m-by-n matrix of zeros that can be used with Forward Diff.
+
+    """
+    function ForwardDiffZeros(m, n; nderivs=0, difforder=0)
+        if nderivs == 0 || difforder == 0
+            return zeros(m, n)
+        elseif difforder == 1
+            return zeros(ForwardDiff.Dual{nderivs, Float64}, m , n)
+        elseif difforder == 2
+            return zeros(ForwardDiff.Dual{nderivs, ForwardDiff.Dual{nderivs, Float64}}, m, n)
+        else
+            error("Don't know how to do that order of derivatives!", nderivs)
+        end
+    end
+          
+else
+    
+    # --------------------------------------------------------------
+    #
+    #         FOR FORWARDDIFF >= 0.6   (Julia 0.6 and onwards)
+    #
+    # --------------------------------------------------------------
+    
+    # No more automatic converts from Dual to Float64; instead, let's use an explicit call to a get_value function.
+    # That means that any accidental casts into Float64s will cause errors, alerting us of 
+    # problems rather than letting the code run but producing derivatives that are zero
+    
+    """
+    v = get_value(x)
+    
+    If you're going to @print something that might be a ForwardDiff Dual, use this function. It'll
+    return the value of the number if it is a Dual and just the number if it was not a Dual, suitable for
+    printing, e.g.
+    
+        @printf("%g\n", get_value(x))
+    
+    will work regardless of whether x is a ForwardDiff Dual, a Float64, or an Int64    
+    """
+    function get_value(x)
+        if typeof(x)<:Array && length(x)>0 
+            if typeof(x[1])<:ForwardDiff.Dual
+                y = zeros(size(x)); 
+                if typeof(x[1].value)<:ForwardDiff.Dual  # nested, taking 2nd derivative
+                    for i=1:length(y); y[i] = x[i].value.value; end;
+                else # not nested, 1st derivative
+                    for i=1:length(y); y[i] = x[i].value; end;
+                end
+                return y
+            elseif typeof(x[1])<:Int64 || typeof(x[1])<:Float64
+                return x
+            else
+                error(@sprintf("Don't know how to get the value of type %s", typeof(x[1])))
+            end
+        elseif typeof(x)<:Array
+            return zeros(size(x))
+        elseif typeof(x)<:ForwardDiff.Dual
+            if typeof(x.value)<:ForwardDiff.Dual; return x.value.value; else; return x.value; end;
+        elseif typeof(x)<:Int64 || typeof(x)<:Float64
+            return x
+        else
+            error(@sprintf("Don't know how to get the value of type %s", typeof(x)))
+        end
+    end    
+    
+
+    """
+    e = get_eltype(vars)
+    
+    vars should be a tuple of variables. If any of them is a ForwardDiff Dual, this function returns
+    the typeof of that one (the first one encountered); otherwise it returns Float64.
+    """
+    function get_eltype(vars)
+        for v in vars
+            if typeof(v)<:ForwardDiff.Dual
+                return typeof(v)
+            end
+        end
+        return Float64
+    end
+
 end
 
 
-"""
-function M = ForwardDiffZeros(m, n; nderivs=0, difforder=0)
-
-Use instead of zeros(). Creates a matrix of zeros, of size m rows by n columns, with elements appropriate for 
-differentiation by ForwardDiff. If nderivs==0 or difforder==0 then the elements will be regular
-Float64, not ForwardDiff types.
-
-PARAMETERS:
-===========
-
-m        Integer, number of rows
-
-n        Integer, number of columns
-
-
-OPTIONAL PARAMETERS:
-====================
-
-nderivs=0       The number of variables that we'll be differentiating with respect to. In other
-                words, this number is equal to the length of the gradient. If this is left as zero (the default) then 
-                the data type will be regular Float64
-
-difforder=0     The order of the derivative we will want to take.  Zero means nothing, stick with
-                regular Float64, 1 means gradient, 2 means hessian
-
-RETURNS:
-========
-
-An m-by-n matrix of zeros that can be used with Forward Diff.
-
-"""
-function ForwardDiffZeros(m, n; nderivs=0, difforder=0)
-    if nderivs == 0 || difforder == 0
-        return zeros(m, n)
-    elseif difforder == 1
-        return zeros(ForwardDiff.Dual{nderivs, Float64}, m , n)
-    elseif difforder == 2
-        return zeros(ForwardDiff.Dual{nderivs, ForwardDiff.Dual{nderivs, Float64}}, m, n)
-    else
-        error("Don't know how to do that order of derivatives!", nderivs)
-    end
-end
-      
 
 
 # DON'T MODIFY THIS FILE -- the source is in file Cost Function Minimization and Hessian Utilities.ipynb
@@ -154,77 +244,176 @@ end
 
 using ForwardDiff
 
-"""
-function value, gradient, hessian = vgh(func, x0)
+if FDversion() < 0.6
+    # --------------------------------------------------------------
+    #
+    #               FOR FORWARDDIFF < 0.6   (Julia 0.5.2)
+    #
+    # --------------------------------------------------------------
 
-Wrapper for ForwardDiff.hessian!() that computes and returns all three of a function's value, gradient, and hessian.
+    using DiffBase
+    """
+    function value, gradient, hessian = vgh(func, x0)
 
-EXAMPLE:
-========
+    Wrapper for ForwardDiff.hessian!() that computes and returns all three of a function's value, gradient, and hessian.
 
-function tester(x::Vector)
+    EXAMPLE:
+    ========
 
-    return sum(x.*x)
-end
+    function tester(x::Vector)
 
-value, grad, hess = vgh(tester, [10, 3.1])
-"""
-function vgh(func, x0)
-    out = DiffBase.HessianResult(x0)             
-    ForwardDiff.hessian!(out, func, x0)
-    value    = DiffBase.value(out)
-    gradient = DiffBase.gradient(out)
-    hessian  = DiffBase.hessian(out)
+        return sum(x.*x)
+    end
+
+    value, grad, hess = vgh(tester, [10, 3.1])
+    """
+    function vgh(func, x0)
+        out = DiffBase.HessianResult(x0)             
+        ForwardDiff.hessian!(out, func, x0)
+        value    = DiffBase.value(out)
+        gradient = DiffBase.gradient(out)
+        hessian  = DiffBase.hessian(out)
+
+        return value, gradient, hessian    
+    end
+
+
+
+
+
+    """
+    function value, gradient, hessian = keyword_vgh(func, args, x0)
+
+    Wrapper for vgh() that computes and returns all three of a function's value, gradient, and hessian, but now
+    uses make_dict() to apply it to a function that only takes keyword-value pairs. 
+
+    *Note that func MUST also take the keyword parameters nderivs and difforder*. If you declare any vectors or 
+    matrices inside func() (or inside any function inside func()), use ForwardDiffZeros with these two parameters, 
+    do NOT use zeros(). Your gradients will come out as zero is you use zeros().
+
+    # PARAMETERS
+
+    * func    A function that takes keyword-value pairs only, including nderivs and difforder.  I.e., it must be a function declared as `function func(; nderivs=0, difforder=0, other_kw_value_pairs)` or as `function func(; nderivs=0, difforder=0, other_kw_value_pairs_dict...)`
+    * args    A list of strings indicating names of variables to work with
+    * x0      A vector with the value of the variables indicates in args.  **See make_dict() for how to pass both scalars and vectors as variables**
+
+    # IMPORTANT JULIA BUG
+
+    If you modify func, it is possible that keyword_vgh() will still work on the previously defined version. AACK!  
+    That's horrible! Alice Yoon's tip on the workaround: instead of func(), use (;params...) -> func(; params...) and then
+    everything will be fine. Perhaps this bug will be fixed in Julia 0.6
+
+    # EXAMPLE:
+
+    function tester(;a=10, b=20, c=30, nderivs=0, difforder=0)
+        M = ForwardDiffZeros(3, 3; nderivs=nderivs, difforder=difforder)
+        M[1,1] = a^2*10
+        M[2,2] = b*20
+        M[3,3] = a*sqrt(c)*30.1
+        return trace(M)
+    end
+
+    value, grad, hess = keyword_vgh(tester, ["a", "c"], [10, 3.1])
+
+    value, grad, hess = keyword_vgh((;params...) -> tester(;params...), ["a", "c"], [10, 3.1])
+
+    """
+    function keyword_vgh(func, args, x0)
+
+        value, gradient, hessian = vgh(x -> func(;nderivs=length(x), difforder=2, make_dict(args, x)...), x0)
+
+        return value, gradient, hessian    
+    end
     
-    return value, gradient, hessian    
-end
+    
+else    
+    # --------------------------------------------------------------
+    #
+    #         FOR FORWARDDIFF >= 0.6   (Julia 0.6 and onwards)
+    #
+    # --------------------------------------------------------------
+
+    using DiffResults
+    
+    """
+    function value, gradient, hessian = vgh(func, x0)
+
+    Wrapper for ForwardDiff.hessian!() that computes and returns all three of a function's value, gradient, and hessian.
+
+    EXAMPLE:
+    ========
+
+    function tester(x::Vector)
+
+        return sum(x.*x)
+    end
+
+    value, grad, hess = vgh(tester, [10, 3.1])
+    """
+    function vgh(func, x0)
+        out = DiffResults.HessianResult(x0)             
+        out = ForwardDiff.hessian!(out, func, x0)
+        value    = DiffResults.value(out)
+        gradient = DiffResults.gradient(out)
+        hessian  = DiffResults.hessian(out)
+
+        return value, gradient, hessian    
+    end
 
 
 
 
 
-"""
-function value, gradient, hessian = keyword_vgh(func, args, x0)
+    """
+    function value, gradient, hessian = keyword_vgh(func, args, x0)
 
-Wrapper for vgh() that computes and returns all three of a function's value, gradient, and hessian, but now
-uses make_dict() to apply it to a function that only takes keyword-value pairs. 
+    Wrapper for vgh() that computes and returns all three of a function's value, gradient, and hessian, but now
+    uses make_dict() to apply it to a function that only takes keyword-value pairs. 
 
-*Note that func MUST also take the keyword parameters nderivs and difforder*. If you declare any vectors or 
-matrices inside func() (or inside any function inside func()), use ForwardDiffZeros with these two parameters, 
-do NOT use zeros(). Your gradients will come out as zero is you use zeros().
+    *Note that if you declare any vectors or matrices inside func() (or inside any function inside func()), 
+    you will need to make sure they are ForwardDiff Duals if you want to differentiate w.r.t. their contents.*
+    The function get_eltype()  can help you with this.  For example, if you have three variables, a, b, and c,
+    and you don't know in advance which one you will differentiate w.r.t., you could declare a matrix using
+    
+        new_matrix = zeros(get_eltype((a,b,c)), 2, 3)
+    
+    and that will make sure that it is the right type if you later want derivatives of contents of that 
+    matrix.
 
-# PARAMETERS
+    # PARAMETERS
 
-* func    A function that takes keyword-value pairs only, including nderivs and difforder.  I.e., it must be a function declared as `function func(; nderivs=0, difforder=0, other_kw_value_pairs)` or as `function func(; nderivs=0, difforder=0, other_kw_value_pairs_dict...)`
-* args    A list of strings indicating names of variables to work with
-* x0      A vector with the value of the variables indicates in args.  **See make_dict() for how to pass both scalars and vectors as variables**
+    * func    A function that takes keyword-value pairs only, including nderivs and difforder.  I.e., it must be a function declared as `function func(; nderivs=0, difforder=0, other_kw_value_pairs)` or as `function func(; nderivs=0, difforder=0, other_kw_value_pairs_dict...)`
+    * args    A list of strings indicating names of variables to work with
+    * x0      A vector with the value of the variables indicates in args.  **See make_dict() for how to pass both scalars and vectors as variables**
 
-# IMPORTANT JULIA BUG
+    # IMPORTANT JULIA BUG
 
-If you modify func, it is possible that keyword_vgh() will still work on the previously defined version. AACK!  
-That's horrible! Alice Yoon's tip on the workaround: instead of func(), use (;params...) -> func(; params...) and then
-everything will be fine. Perhaps this bug will be fixed in Julia 0.6
+    If you modify func, it is possible that keyword_vgh() will still work on the previously defined version. AACK!  
+    That's horrible! Alice Yoon's tip on the workaround: instead of func(), use (;params...) -> func(; params...) and then
+    everything will be fine. Not sure yet whether this bug is fixed in Julia 0.6
 
-# EXAMPLE:
+    # EXAMPLE:
 
-function tester(;a=10, b=20, c=30, nderivs=0, difforder=0)
-    M = ForwardDiffZeros(3, 3; nderivs=nderivs, difforder=difforder)
-    M[1,1] = a^2*10
-    M[2,2] = b*20
-    M[3,3] = a*sqrt(c)*30.1
-    return trace(M)
-end
+    function tester(;a=10, b=20, c=30)
+        M = zeros(get_eltype((a,b,c)), 3, 3)
+        M[1,1] = a^2*10
+        M[2,2] = b*20
+        M[3,3] = a*sqrt(c)*30.1
+        return trace(M)
+    end
 
-value, grad, hess = keyword_vgh(tester, ["a", "c"], [10, 3.1])
+    value, grad, hess = keyword_vgh(tester, ["a", "c"], [10, 3.1])
 
-value, grad, hess = keyword_vgh((;params...) -> tester(;params...), ["a", "c"], [10, 3.1])
+    value, grad, hess = keyword_vgh((;params...) -> tester(;params...), ["a", "c"], [10, 3.1])
 
-"""
-function keyword_vgh(func, args, x0)
+    """
+    function keyword_vgh(func, args, x0)
 
-    value, gradient, hessian = vgh(x -> func(;nderivs=length(x), difforder=2, make_dict(args, x)...), x0)
+        value, gradient, hessian = vgh(x -> func(;make_dict(args, x)...), x0)
 
-    return value, gradient, hessian    
+        return value, gradient, hessian    
+    end
+    
 end
 
 
@@ -232,11 +421,11 @@ end
 
 
 
-#############################################################################
-#                                                                           #
-#      OLD AND LARGELY UNUSED KEYWORD GRADIENTS AND HESSIANS                #
-#                                                                           #
-#############################################################################
+##############################################################################################
+#                                                                                            #
+#      OLD AND LARGELY UNUSED AND UNMAINTAINED KEYWORD GRADIENTS AND HESSIANS                #
+#                                                                                            #
+##############################################################################################
 
 
 """
@@ -813,11 +1002,19 @@ end
 
 
 
+# Julia 0.5 returns an error if you run Pkg.installed on a package that has not been installed
 try
     Pkg.installed("JLD")
 catch
     Pkg.add("JLD")
 end
+
+# Julia 0.6 does not crash but returns a Void if you run Pkg.installed on a package that has not been installed
+if isa(Pkg.installed("JLD"), Void)
+    Pkg.add("JLD")
+end
+
+
 using JLD
 using MAT
 
