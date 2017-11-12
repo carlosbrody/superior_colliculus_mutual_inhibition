a1671e83254967ab93ae20a8f12729d6ff7a55a4
Unknown <carlosbrody@gmail.com>
2017-11-12 15:09:48 -0500

    Script, kilsl all processes matching given name. Use alone for help info

---
+++ kill_by_name.sh
@@ -0,0 +1,11 @@
+#! /bin/csh
+
+# This script goes kills -9 ALL processes matching a name. Crazy dangerous!
+
+if ( $#argv != 1 ) then
+    echo Sorry, need one arguments, the name of the process(es) to be killed
+    echo For example:  ./kill_by_name.sh julia
+    exit
+endif
+
+ps -ef | grep $argv[1] | grep -v grep | awk '{print $2}' | xargs kill -9
