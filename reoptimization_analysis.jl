ea818f20c6b386b268fc1a810637eb40acaeb498
Unknown <carlosbrody@gmail.com>
2017-12-06 12:48:24 -0500

    doc tweak

--- reoptimization_analysis.jl
+++ reoptimization_analysis.jl
@@ -21,35 +21,38 @@ if !isdefined(:resn) || true   # forcing it to run right now so we always get la
     resn = farmload("C17"; farmdir="MiniOptimized_Redux")
 end
 
-# 
-cost_choice = "cost"
-threshold   = -0.0001
+# The next two lines define how we threshold the optimized runs, to choose which ones go into the PCA plot
+cost_choice = "cost"     # "cost" means test cost, "tcost" means training cost
+threshold   = -0.0001    # Any run with a cost below this goes in
 
-paramso = reso["params"]
+# Next we compute PCA space.
+paramso = reso["params"][find(res[cost_choice].<=threshold),:]
+# Whiten (mean zero, std 1):
 paramso = paramso - ones(size(paramso,1),1)*mean(paramso,1)
 paramso = paramso ./ (ones(size(paramso,1),1)*std(paramso,1))
-
+# compute and diagonalize covariance matrix:
 C = (paramso' * paramso)/size(paramso,1)
 D, V = eig(C)
-
+# Project old and new parameters onto eigenvectors, reverse so top PCA is first
 oVparams = (inv(V)*reso["params"]')'[:,end:-1:1]
 nVparams = (inv(V)*resn["params"]')'[:,end:-1:1]
 
-figure(1); clf(); iold = inew = 0
+figure(1); clf(); 
 
+# Plot a red dot for all the old parameters:
 u = find(res[cost_choice].<=threshold)
 plot(oVparams[u,1], oVparams[u,2], "r.", markersize=11)
 
-nloop = hg = hr = 1
+# Now plot a green dot and a connecting line for all the new ones:
 for f in readdir("MiniOptimized_Redux/")
-    if startswith(f, "farm_C17") # && nloop > 10
+    if startswith(f, "farm_C17") 
         iold = find(reso["files"] .== ("MiniOptimized/" * f))
         inew = find(resn["files"] .== ("MiniOptimized_Redux/" * f))
         hg = plot([oVparams[iold,1], nVparams[inew,1]], [oVparams[iold,2], nVparams[inew,2]], 
             "g.-", markersize=11)
+        # Add another red dot on top of old values to keep it red
         hr = plot(oVparams[iold,1], oVparams[iold,2], "r.-", markersize=11)
     end
-    nloop += 1
 end
 
 xlabel("PCA Dim 1")
