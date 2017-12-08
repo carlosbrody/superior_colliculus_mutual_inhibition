3e2c3d7353bf46f713b5ef7d925a657518911a71
Unknown <carlosbrody@gmail.com>
2017-12-08 15:27:42 -0500

    Now allow specifying farm directory as a parameter

--- pull_results.sh
+++ pull_results.sh
@@ -3,13 +3,28 @@
 # This script goes into a running gcloud VM, adds, commits, and pushes the latest farm results, and then comes
 # back to the local machine and executes git pull to get all the results.
 
-if ( $#argv != 2 ) then
+if ( $#argv < 2 ) then
     echo Sorry, need two arguments, the name of the gcloud machine and the letter of the farm being created there
-    echo For example:  ./pull_results.sh proanti003 D
+    echo For example:  ./pull_results.sh proanti003 D 
+    echo "If a third argument is present, it is taken to be the directory where all this will take place; default is FarmFields"
     exit
 endif
 
-gcloud compute ssh --zone="us-east1-c" --command="cd superior_colliculus_mutual_inhibition/; git pull; git add FarmFields/farm_$argv[2]_*; git commit -m "'"'"The latest from farm $argv[2]"'"'"  ; git push origin master" carlosbrody@$argv[1] 
+if ( $#argv >= 3 ) then
+    set farmdir = $argv[3]
+else
+    set farmdir = "FarmFields"
+endif
+
+set rootdir = superior_colliculus_mutual_inhibition
+if ( $argv[1] == "proanti024" ) then
+    set zone = "us-east1-d"
+else
+    set zone = "us-east1-c"
+endif
+
+
+gcloud compute ssh --zone=$zone --command="cd $rootdir/; git pull; git add $farmdir/farm_$argv[2]_*; git commit -m "'"'"The latest from $farmdir ::$argv[2]"'"'"  ; git push origin master" carlosbrody@$argv[1] 
 
 git pull
  
