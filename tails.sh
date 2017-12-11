38f4e45c588e53773c1bdbe163eab653b2ca1aa1
Carlos Brody <carlosbrody@gmail.com>
2017-12-11 17:16:44 -0500

    generalizing tails

--- tails.sh
+++ tails.sh
@@ -1,9 +1,15 @@
 #! /bin/csh
 
+if ( `hostname` == brody-t2e ) then
+    set hostnumber = "t2e"
+else
+    set hostnumber = `hostname | sed s/proanti//`
+endif
+
+
 set j=1
-while ( $j <= "30" )
-      grep "] [0-9][0-9]:" Reports029/report_out_$j | tail -1
-      @ j++
+foreach f( `find ../Reports$hostnumber -name "report_*"` )
+      grep "] [0-9][0-9]:" $f | tail -1
 end
 
 
