9078a7d73596f4fc3a43238e64b1f8acfeddefd4
Unknown <carlosbrody@gmail.com>
2017-12-19 12:21:25 -0500

    Turned it into a function

--- LDA_browser.jl
+++ LDA_browser.jl
@@ -57,13 +57,16 @@ expressions, evaluated in a context where the following variables exist:
 
 - xlb     xlims box. Write in "[min, max]" if you want particular horizontal axis limits
           for the various plots.
+
+- npla    Number of "plottables" boxes
+
 """
 function setup_control_figure(fignum)
     figure(fignum); clf();
     
     # rad: plot farm versus not
     ax1 = axes([0.01, 0.84, 0.37, 0.15])
-    rad = kbMonitorModule.radio_buttons(ax1, ["Don't plot trials", "Plot trials (wait for it)"])
+    rad = kbMonitorModule.radio_buttons(ax1, ["Plot trials (wait for it)", "Don't plot trials"])
 
     # heb : plot all or hits only or err only
     ax0 = axes([0.5, 0.8, 0.37, 0.19])
@@ -104,7 +107,7 @@ function setup_control_figure(fignum)
     ax5 = axes([0.68, axy, 0.3, axheight])   # working on adding xlims control
     xlb = kbMonitorModule.text_box(ax5, "xlims", "")
 
-    return rad, heb, plb, ylb, slb, dbx, tbx, xlb
+    return rad, heb, plb, ylb, slb, dbx, tbx, xlb, npla
 end
 
 
@@ -121,166 +124,170 @@ end
 using MultivariateStats
 using Clustering
 
-farmid  = "C17"
-farmdir = "MiniOptimized" ; nclusters = 3
-# farmdir = "../C17_Optimized_B"; nclusters = 5;
-
 if !isdefined(:histo_params)
     @printf("Loading results_analysis.jl\n")
     include("results_analysis.jl")
 end
 
-if !isdefined(:res) || true
-    res = farmload(farmid, verbose=true, farmdir=farmdir)
-    res["params"][:,4] = abs.(res["params"][:,4])
-end
-
-pygui(true); 
-remove_all_BPs(); # Clean up any previous links between clicks on figures and callback functions
-plt[:close](1); plt[:close](2); plt[:close](3); plt[:close](4); plt[:close](5)
-
-# Carlos' favored configuration, but adjust to suit -- use capture_current_figure_configuration() 
-# to see code that reproduces a configuration you like once you find it
-
-figure(1); set_current_fig_position(1325, 41, 640, 672)   # x, y, width, height
-figure(2); set_current_fig_position(645, 785, 680, 408)
-figure(3); set_current_fig_position(0, 785, 641, 407)
-figure(4); set_current_fig_position(3, 23, 1288, 797)
-figure(5); set_current_fig_position(1338, 736, 540, 450)
-
-# -------------- SET UP CONTROL FIGURE --------
-
-rad, heb, plb, ylb, slb, dbx, tbx, xlb = setup_control_figure(5)
-
-##########################################################
-#
-#   next couple of lines define cost type and threshold
-#
-##########################################################
-
-cost_choice = "cost"   # "cost" is test cost, "tcost" is training cost
-threshold   = -0.0002  # Below this is a "successful" run
+function LDA_browser(farmid, farmdir, threshold=-0.0002, cost_choice="cost", nclusters=4)
+# farmid  = "C17"; farmdir = "Mini_C17_Optimized_B"
+# farmid  = "C19"; farmdir = "Mini_C19"
+# farmid  = "C17"; farmdir = "C17_Optimized_A"
 
 
-nsucc = length(find(res[cost_choice].<threshold))
-@printf("\n%d runs being plotted as successful, with cost `%s' less than %g\n\n", nsucc, cost_choice, threshold)
+    if !isdefined(:res) || true
+        res = farmload(farmid, verbose=true, farmdir=farmdir)
+        res["params"][:,4] = abs.(res["params"][:,4])
+    end
 
-# Put up the histograms
-HD = histo_params(res; threshold=threshold, cost_choice=cost_choice);
-pause(0.001)
+    pygui(true); 
+    remove_all_BPs(); # Clean up any previous links between clicks on figures and callback functions
+    plt[:close](1); plt[:close](2); plt[:close](3); plt[:close](4); plt[:close](5)
 
-# The callback function that will be called after clicking on a data dot:
-function highlight_all(fname, LD, SV)
-    PCA_highlight(fname, SV);       # Color the selected dots in the SV plot
-    scatter_highlight(fname, LD);   # Color the selected dots in the LDA plot
-    histo_highlight(fname, HD)      # Color the selected bars in the histograms
-    pause(0.001);   # We don't really care about the 1 ms pause; just a convenient way to flush all pending graphics commandsj
+    # Carlos' favored configuration, but adjust to suit -- use capture_current_figure_configuration() 
+    # to see code that reproduces a configuration you like once you find it
 
-    if rad[:value_selected] == "Plot trials (wait for it)"
-        if ~isnull(tryparse(Int64, tbx[:text]));               ntrials = parse(Int64, tbx[:text])
-        else; @printf("Couldn't parse the ntrials to plot\n"); ntrials = 10
-        end
-        plottables = Array{String}(0,1); ylims = Array{Any}(0,1); ylabels = Array{String}(0,1)
-        for i=npla:-1:1
-            if plb[i][:text] != ""
-                plottables = [plottables ; plb[i][:text]]
-                ylabels    = [ylabels    ; plb[i][:text]]
-                if ylb[i][:text] == ""
-                    ylims = [ylims ; nothing]
-                else
-                    ylims = [ylims ; [eval(parse(ylb[i][:text]))]]
-                end                
+    figure(1); set_current_fig_position(1325, 41, 640, 672)   # x, y, width, height
+    figure(2); set_current_fig_position(645, 785, 680, 408)
+    figure(3); set_current_fig_position(0, 785, 641, 407)
+    figure(4); set_current_fig_position(3, 23, 1288, 797)
+    figure(5); set_current_fig_position(1338, 736, 540, 450)
+
+    # -------------- SET UP CONTROL FIGURE --------
+
+    rad, heb, plb, ylb, slb, dbx, tbx, xlb, npla = setup_control_figure(5)
+
+    nsucc = length(find(res[cost_choice].<threshold))
+    @printf("\n%d runs being plotted as successful, with cost `%s' less than %g\n\n", nsucc, cost_choice, threshold)
+
+    # Put up the histograms
+    HD = histo_params(res; threshold=threshold, cost_choice=cost_choice);
+    pause(0.001)
+
+    # The callback function that will be called after clicking on a data dot:
+    function highlight_all(fname, SV_space, PARAM_space)
+        scatter_highlight(fname, SV_space);      # Color the selected dots in the LDA plot
+        scatter_highlight(fname, PARAM_space);   # Color the selected dots in the LDA plot
+        histo_highlight(fname, HD)               # Color the selected bars in the histograms
+        pause(0.001);   # We don't really care about the 1 ms pause; just a convenient way to flush all pending graphics commandsj
+
+        if rad[:value_selected] == "Plot trials (wait for it)"
+            if ~isnull(tryparse(Int64, tbx[:text]));               ntrials = parse(Int64, tbx[:text])
+            else; @printf("Couldn't parse the ntrials to plot\n"); ntrials = 10
+            end
+            plottables = Array{String}(0,1); ylims = Array{Any}(0,1); ylabels = Array{String}(0,1)
+            for i=npla:-1:1
+                if plb[i][:text] != ""
+                    plottables = [plottables ; plb[i][:text]]
+                    ylabels    = [ylabels    ; plb[i][:text]]
+                    if ylb[i][:text] == ""
+                        ylims = [ylims ; nothing]
+                    else
+                        ylims = [ylims ; [eval(parse(ylb[i][:text]))]]
+                    end                
+                end
+            end
+            xlims = xlb[:text]=="" ? nothing : eval(parse(xlb[:text]))
+            if heb[:value_selected]     == "all";      hstyle="-"; estyle="--";
+            elseif heb[:value_selected] == "hit only"; hstyle="-"; estyle="";
+            else                                       hstyle="";  estyle="--";
+            end
+            selectize = slb[:text]=="" ? "true" : slb[:text]
+            try
+                plot_farm(fname, testruns=ntrials, fignum=4, 
+                    plottables=plottables, ylabels=ylabels, ylims=ylims, xlims=xlims,
+                    hit_linestyle = hstyle, err_linestyle = estyle, selectize=selectize,
+                    overrideDict=eval(parse("Dict("*dbx[:text]*")"))) 
+            catch e
+                @printf("Couldn't plot farm, error %s\n", e)
+                catch_stacktrace()
             end
         end
-        xlims = xlb[:text]=="" ? nothing : eval(parse(xlb[:text]))
-        if heb[:value_selected]     == "all";      hstyle="-"; estyle="--";
-        elseif heb[:value_selected] == "hit only"; hstyle="-"; estyle="";
-        else                                       hstyle="";  estyle="--";
-        end
-        selectize = slb[:text]=="" ? "true" : slb[:text]
-        try
-            plot_farm(fname, testruns=ntrials, fignum=4, 
-                plottables=plottables, ylabels=ylabels, ylims=ylims, xlims=xlims,
-            hit_linestyle = hstyle, err_linestyle = estyle, selectize=selectize,
-                overrideDict=eval(parse("Dict("*dbx[:text]*")"))) 
-        catch e
-            @printf("Couldn't plot farm, error %s\n", e)
-            catch_stacktrace()
-        end
     end
-end
 
 
-# Put up the LDA plot
-use_marino = false   # if true, loads the cluster labels and linear discriminant projections from the output of Marino's matlab code
-if use_marino
-    G = matread("compute_clustering/LDA_output.mat")
-    idx = find(G[cost_choice].<=threshold)
-    G["cluster_ids"] = G["cluster_ids"][idx]
-    G["ldparams"]    = G["ldparams"][:,idx]
-    G["files"]       = G["files"][idx]
-    set_indices = [find(G["cluster_ids"].==1), find(G["cluster_ids"].==2), find(G["cluster_ids"].==3)]
-    LD = interactive_scatters(G["ldparams"]', G["files"], set_indices=set_indices, plot_set2=true,
-        user_callback=(fname, Trash) -> highlight_all(fname, LD, SV), fignum=2, axisDims = [1 2]);
-else # compute cluster labels and linear discriminant projections ourselves
-    G1 = load(farmdir*farmid*"_SVD_response_matrix3.jld")   # these include the trial-averaged PSTHS Alex produced
-    idx = find(G1["results"][cost_choice].<=threshold)       # choose runs
-    dynamics_data = G1["response"][idx,:]                    # get the PSTHS
-    fnames = G1["results"]["files"][idx]                     #    and their corresponding filenames
-    params = G1["results"]["params"][idx,:]                  #    and their corresponding parameter values
+    # -----------------  Put up the LDA plot  ---------
+    # We'll use the output from Marino's Matlab code to get number of clusters.
+    # The rest happens here.-
+    marino_file = "compute_clustering/MarinoCode_"*farmid*"_"*farmdir*".jld"
+    if isfile(marino_file)
+        nclusters, cost_choice_marino, threshold_marino = 
+            load(marino_file, "ngroups", "cost_choice", "threshold")
+        nclusters=Int64(nclusters)
+        if threshold_marino != threshold || cost_choice_marino != cost_choice
+            warn("Marino file not generated with requested threshold and cost choice!\n", 
+            "Number of clusters may be off.\n")
+        end
+    else
+        warn(@sprintf("Didn't find file %s with output from Marino's code, guessing nclusters=%d\n",
+        marino_file, nclusters))
+    end
 
-    cluster_ids = assignments(kmeans(dynamics_data', nclusters, init=:kmcen))   # label each run with a cluster id
-    
-    M = fit(SubspaceLDA, params', nclusters, cluster_ids)            # do multiclass LDA in param space with those cluster ids
-    ld_data = transform(M, params')'                         # get the parameters in the LDA projection
 
-    # Now put it on the screen
+    alexfile = farmdir*"_"*farmid*"_SVD_response_matrix3_reduced.jld"
+    results, response= load(alexfile, "results", "response")   # these include the trial-averaged PSTHS Alex produced
+    idx       = find((results[cost_choice].<threshold)  .&  .!any(isnan.(response),2));
+    files     = results["files"][idx]
+    params    = results["params"][idx,:]
+    # Hack params so that sigma is always positive
+    args      = load(files[1], "args")
+    params[:, find(args.=="sigma")] = abs.(params[:, find(args.=="sigma")])
+    # Zero-mean the responses
+    response  = response[idx,:]
+    response  = response - ones(size(response,1),1)*mean(response,1)
+
+    U, S, V = svd(response)
+
+    # We arbitrarily use the top 4 principal components:
+    ncomponents=4
+    D = response*V[:,1:ncomponents]
+    cluster_ids = assignments(kmeans(D', nclusters, init=:kmcen)) # , display=:iter))
     set_indices = Array{Any}(nclusters)
-    for i=1:nclusters
-        set_indices[i] = find(cluster_ids.==i)
-    end
-    if nclusters==3; axisDims=[1 2]; else axisDims=[1 2 ; 3 4]; end
-    LD = interactive_scatters(ld_data, fnames, set_indices=set_indices, plot_set2=true,
-        user_callback=(fname, Trash) -> highlight_all(fname, LD, SV), 
-        fignum=2, axisDims = axisDims);
-    
+    for i=1:nclusters; set_indices[i] = find(cluster_ids.==i); end
+
+
+    # Scatterplot in singular value space
+    SV_space = interactive_scatters(D, files, set_indices=set_indices, plot_set2=true, 
+        fignum=3, axisDims=[1 2], 
+        user_callback=(fname, Trash) -> highlight_all(fname, SV_space, PARAM_space));
+
+    # Now do LDA in parameter space, and put up scatter plot in parameter space
+    M = fit(SubspaceLDA, params', nclusters, cluster_ids) # do multiclass LDA in param space with those cluster ids
+    ld_data = transform(M, params')'                         # get the parameters in the LDA projection
+    PARAM_space = interactive_scatters(ld_data, files, set_indices=set_indices, plot_set2=true,
+        fignum=2, axisDims = [2 1; 3 1],
+        user_callback=(fname, Trash) -> highlight_all(fname, SV_space, PARAM_space));
+
+
+
+    # Print out some instructions for the user
+    docstring = """
+
+    Wait for PCA and SVD plots to come up.
+    Then click on any dot within the PCA plot or SVD plots to
+    see the corresponding data in the other plots. Click on
+    'Don't plot trials' in figure 5 if you want to go faster,
+    without running trials.
+
+    The window placement fits a 15-in Macbook Pro, but adjust
+    at will. Once you find window positions you like, run
+        capture_current_figure_configuration()
+    to get copy-pastable code that reproduces it.
+
+    ntrials to run is the number of trials per condition that will
+    be run in plot_farm() to compute %correct. Up to 20 of them
+    will be plotted.
+
+    Override is a dict that will override any paramater values in
+    the plotted trials. For example, if you write
+        :sigma=>0.2, :rule_and_delay_period=>1.6
+    then no matter what the run's file said, if you ask for trials
+    to be plotted, those trials will run
+    with that sigma and that rule_and_delay_period.
+
+    """
+
+    @printf("%s", docstring)
 end
-pause(0.001)
-
-# Put up the SVD plot
-SV = plot_SVD(threshold=threshold, cost_choice=cost_choice, 
-    user_callback = (fname, Trash) -> highlight_all(fname, LD, SV),
-    plot_unsuccessful=false, compute_good_only=false, fignum=3);
-
-
-# Print out some instructions for the user
-docstring = """
-
-Wait for PCA and SVD plots to come up.
-Then click on any dot within the PCA plot or SVD plots to
-see the corresponding data in the other plots. Click on
-'Don't plot trials' in figure 5 if you want to go faster,
-without running trials.
-
-The window placement fits a 15-in Macbook Pro, but adjust
-at will. Once you find window positions you like, run
-    capture_current_figure_configuration()
-to get copy-pastable code that reproduces it.
-
-ntrials to run is the number of trials per condition that will
-be run in plot_farm() to compute %correct. Up to 20 of them
-will be plotted.
-
-Override is a dict that will override any paramater values in
-the plotted trials. For example, if you write
-    :sigma=>0.2, :rule_and_delay_period=>1.6
-then no matter what the run's file said, if you ask for trials
-to be plotted, those trials will run
-with that sigma and that rule_and_delay_period.
-
-"""
-
-@printf("%s", docstring)
-
 
 
