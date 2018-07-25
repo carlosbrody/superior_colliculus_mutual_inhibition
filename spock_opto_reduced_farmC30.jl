ec8788a26fd19469449b62d000772142e0d3cd1e
Alex Piet <alexpiet@gmail.com>
2018-07-25 10:57:35 -0400

    cleaning up debugging comments

--- spock_opto_reduced_farmC30.jl
+++ spock_opto_reduced_farmC30.jl
@@ -7,19 +7,13 @@ Farm C30: Just like C19, but with an unsymmetrized W
 
 """
 
-println("here1")
 if length(ARGS)>0  &&  ~isnull(tryparse(Int64, ARGS[1])); 
-println("here2")
     my_run_number = parse(Int64, ARGS[1]); 
-println("here3")
 else                                                      
     my_run_number = 1; # I am process my_run_number
 end
-println("here4")
 if length(ARGS)>1  &&  ~isnull(tryparse(Int64, ARGS[2])); 
-println("here5")
     tot_n_runs    = parse(Int64, ARGS[2]); 
-println("here6")
 else
     tot_n_runs = 1;   # I'm being run as part of tot_n_run processes
 end
