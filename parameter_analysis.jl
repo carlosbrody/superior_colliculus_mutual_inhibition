faad693e32ecc5f6be3d3d2da064a7540a876e7b
Alex Piet <alexpiet@gmail.com>
2018-07-25 16:11:10 -0400

    added documentation on scatter_by_args

--- parameter_analysis.jl
+++ parameter_analysis.jl
@@ -10,7 +10,6 @@
 #
 
 
-
 """
     scatter_by_arg(results, args, arg1, arg2)
 
@@ -28,9 +27,40 @@
     args    = f1["args"];
     results = load_farm_cost_filter("C30", "MiniC30")
     scatter_by_arg(results, args, "hW_A","hW_P");
+    
+    #to include cluster labels
+    cluster_info    = load("MiniC30_C30_clusters.jld")
+    cluster_ids     = cluster_info["idx"]
+    scatter_by_arg(results, args, "hW_A", "hW_P"; cluster_ids = cluster_ids)
 
 """
-function scatter_by_arg(results, args, arg1,arg2; fignum=1)
+function scatter_by_arg(results, args, arg1,arg2; fignum=1, cluster_ids=[])
+
+    # check for clustering
+    clustering = !isempty(cluster_ids);
+
+    if !clustering
+        scatter_by_arg_backend(results, args, arg1, arg2; fignum=fignum);
+    else
+        all_colors = "bgrcmyk";
+        # iterate over clusters
+        ids = sort(unique(cluster_ids));
+        for i=1:length(ids)
+            # check for NaN
+            if !isnan(ids[i])
+                # filter results, and call backend
+                fresults    = filter_results_by_cluster(results,cluster_ids, ids[i]);
+                color       = string(all_colors[i]);
+                plot_lines  = i==1;
+                scatter_by_arg_backend(fresults, args, arg1, arg2; fignum=fignum, color=color, plot_lines=plot_lines);
+            end
+        end
+    end
+end
+
+
+# helper function
+function scatter_by_arg_backend(results, args, arg1, arg2; fignum=1, color="k", plot_lines=true);
 
     # get index of each parameter from args
     index1 = find(args .== arg1);
@@ -62,15 +92,39 @@ function scatter_by_arg(results, args, arg1,arg2; fignum=1)
     maxy = maximum(y);
     minx = minimum(x);
     miny = minimum(y);
-    
+    maxx = 3; maxy = 3;
+    minx = -3; miny = -3;    
+
     # plot axis lines
-    plot(vec([0 0]), vec([miny maxy]), "k--");   
-    plot(vec([minx maxx]), vec([0 0]), "k--");
+    if plot_lines
+        plot(vec([0 0]), vec([miny maxy]), "k--");   
+        plot(vec([minx maxx]), vec([0 0]), "k--");
+    end
 
     # scatter the parameters
-    plot(x,y, "bo");
+    plot(x,y, "o",color=color);
+    
+    ylabel(arg2)
+    xlabel(arg1)
 
 end
 
 
+function filter_results_by_cluster(results, cluster_ids, target_cluster)
+    
+    # make index of good clusters
+    gooddex = cluster_ids .== target_cluster
+
+    # filter
+    r1 = Dict();
+    r1["cost"]  = results["cost"][vec(gooddex)];
+    r1["tcost"] = results["tcost"][vec(gooddex)];
+    r1["files"] = results["files"][vec(gooddex)];
+    r1["dirs"]  = results["dirs"][vec(gooddex)];
+    r1["params"]= results["params"][vec(gooddex),:]
+
+    # return filtered matrix
+    return r1
+
+end
 
