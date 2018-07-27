c17e103903dc92abf0d51677a43117c7364b7db1
Alex Piet <piet@spock-login.pni.princeton.edu>
2018-07-27 17:42:05 -0400

    minor bug in C31_control

--- setup_C31_control.jl
+++ setup_C31_control.jl
@@ -69,12 +69,11 @@ search_conditions = Dict(   # :param    default_start   search_box  bound_box
 :target_period_excitation => [mypars[:target_period_excitation], [-1,     1],  [-30  30]],
 :target_period_anti_excitation   => [mypars[:target_period_anti_excitation],   [-1,     1],  [-30, 30]],
 :const_pro_bias           => [mypars[:const_pro_bias],           [-1,     1],  [-30, 30]],
-:opto_strength            => [mypars[:opto_strength],            [0,      1],  [0,    1]],
 :pro_rule_strength        => [mypars[:pro_rule_strength],        [0,   0.5,],  [0,   30]],
 :anti_rule_strength       => [mypars[:anti_rule_strength],       [0,   0.5,],  [0,   30]],
 )
 
-
+#:opto_strength            => [mypars[:opto_strength],            [0,      1],  [0,    1]],
 
 bbox = Dict()
 for k in keys(search_conditions)
