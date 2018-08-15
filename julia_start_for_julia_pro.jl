d0123b58f7d545c04b10516ff1c2e84a5f17ddbc
Unknown <carlosbrody@gmail.com>
2018-08-15 13:35:16 -0400

    A file that starts JuliaPro 0.6.4 successfully for our ProAnti project analysis

---
+++ julia_start_for_julia_pro.jl
@@ -0,0 +1,24 @@
+# Run this file in julia, and it will add and build all the necessary packages for cost function minimization
+
+# ENV["PYTHON"]="/usr/bin/python"
+# Pkg.add("PyCall")
+# Pkg.build("PyCall")
+# Pkg.add("PyPlot")
+# Pkg.build("PyPlot")
+using PyPlot
+
+# Pkg.add("ForwardDiff")
+Pkg.pin("ForwardDiff", v"0.6.0")  # all the code has been developed with that version
+Pkg.add("DiffBase")
+
+# Pkg.add("HDF5")
+Pkg.add("MAT")
+# Pkg.add("JLD")
+Pkg.add("MultivariateStats")
+Pkg.add("Clustering")
+
+using ForwardDiff
+using MAT
+using JLD
+using MultivariateStats
+using Clustering
