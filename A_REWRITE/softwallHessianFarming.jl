8179479c6ff382e974bdd82cf64805338b479e12
Carlos Brody <carlosbrody@gmail.com>
2020-05-18 00:30:49 -0400

    Now with Hessians

---
+++ A_REWRITE/softwallHessianFarming.jl
@@ -0,0 +1,66 @@
+include("startup.jl")
+include("commonSetup.jl")
+
+extra_pars[:seedrand] = Int64(my_run_number*round(time()*1000))
+Random.seed!(extra_pars[:seedrand])
+
+args, seed, bounder = argsSeedBounder();
+
+func =  x -> JJ(extra_pars[:nPro], extra_pars[:nAnti]; verbose=false,
+    make_dict(args, x, merge(mypars, extra_pars))...)[1]
+
+for looper=1:400
+
+    extra_pars[:seedrand] = Int64(my_run_number*round(time()*1000))
+    Random.seed!(extra_pars[:seedrand])
+    args, seed, bounder = argsSeedBounder();
+
+    bfunc, old2new, new2old = boundedVersion(func, bounder)
+
+    g = x -> ForwardDiff.gradient(bfunc, x)
+    h = x -> ForwardDiff.hessian(bfunc, x)
+
+    ResultsStash = ["seedrand"             "cost"      "minimizer" ;
+                     extra_pars[:seedrand] func(seed)  [seed]]
+
+    println("\n\n**************************\n\n")
+    println("\nStarting seed on loop $looper for ResultsStash:")
+    display(ResultsStash)
+    println("\nInitial cost is ", func(seed))
+    println()
+
+
+
+    result = optimize(
+        bfunc, g, h, # seems overall faster without?
+        old2new(seed), NewtonTrustRegion(), # seems overall faster without?
+        Optim.Options(store_trace=true, show_trace=true,
+            iterations=15000, time_limit=10800);
+        inplace=false);
+
+    truecost = func(new2old(Optim.minimizer(result)))
+    if truecost < 0
+        println("------> NEGATIVE TRUE COST <-------")
+        hostname = chomp(read(`hostname`, String))
+        fp = open("negCosts_$hostname", "a")
+        println(fp, extra_pars[:seedrand], ", ", truecost, ", ")
+        writedlm(fp, new2old(Optim.minimizer(result))[:]', ',')
+        close(fp)
+    end
+
+    end
+    println("With seedrand ", extra_pars[:seedrand], " true cost was ", truecost)
+
+    ResultsStash = vcat(ResultsStash,
+        [extra_pars[:seedrand] Optim.minimum(result) [new2old(Optim.minimizer(result))]])
+
+    println("\nResultsStash:")
+    display(ResultsStash)
+    println()
+
+    println("\nJJ:")
+    display(JJ(extra_pars[:nPro], extra_pars[:nAnti]; asDict=true, verbose=false,
+        make_dict(G["args"], new2old(Optim.minimizer(result)), merge(mypars, extra_pars))...))
+
+    println()
+end
