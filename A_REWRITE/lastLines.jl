28887b22a233ef68ef64241cac3797cb67e0b753
Carlos Brody <carlosbrody@gmail.com>
2020-05-20 18:27:02 -0400

    last line reader

---
+++ A_REWRITE/lastLines.jl
@@ -0,0 +1,15 @@
+using DelimitedFiles
+
+if length(ARGS)>0
+   for i=1:length(ARGS)
+      println("--- $i: ", ARGS[i])
+      A = readdlm(ARGS[i], '\n')
+      print(A[end-1])
+      println(A[end])
+   end
+end
+
+##
+
+# fp = fopen("commonSetup.jl", "r")
+# A = readdlm("commonSetup.jl", '\n')
