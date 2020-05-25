e2956cfef5cde39fa40d2911521ab1121d9f78cc
Carlos Brody <carlosbrody@gmail.com>
2020-05-25 01:02:43 -0400

    minor bug fix

--- A_REWRITE/opto_reduced_farmC32.jl
+++ A_REWRITE/opto_reduced_farmC32.jl
@@ -8,15 +8,15 @@ Farm C32: Just like C30, but with variable delay periods, variable target period
 
 """
 
-if length(ARGS)>0  &&  ~isnull(tryparse(Int64, ARGS[1]));
-    my_run_number = parse(Int64, ARGS[1]);
+if length(ARGS)>0  &&  tryparse(Int64, ARGS[1]) != nothing
+   my_run_number = parse(Int64, ARGS[1]);
 else
-    my_run_number = 1; # I am process my_run_number
+   my_run_number = 1; # I am process my_run_number
 end
-if length(ARGS)>1  &&  ~isnull(tryparse(Int64, ARGS[2]));
-    tot_n_runs    = parse(Int64, ARGS[2]);
+if length(ARGS)>1  &&  tryparse(Int64, ARGS[2]) != nothing
+   tot_n_runs    = parse(Int64, ARGS[2]);
 else
-    tot_n_runs = 1;   # I'm being run as part of tot_n_run processes
+   tot_n_runs = 1;   # I'm being run as part of tot_n_run processes
 end
 
 
