0083f5544074cef88527846d9a217a4652301e21
Carlos Brody <carlosbrody@gmail.com>
2020-08-04 01:02:55 -0400

    in major overhaul to allow respawning
    Next thing to fix is starting iteration number after a respawn

--- A_REWRITE/sixNode_reduced_farmC32.jl
+++ A_REWRITE/sixNode_reduced_farmC32.jl
@@ -8,28 +8,57 @@ Six Node C32: Just like C32, but with two Anti nodes per side of the brain
 
 """
 
-if length(ARGS)>0  &&  tryparse(Int64, ARGS[1]) != nothing
-   my_run_number = parse(Int64, ARGS[1]);
-else
-   my_run_number = 1; # I am process my_run_number
+s = ArgParseSettings()
+@add_arg_table! s begin
+    "--report_file"
+        help = "the string representing where the output will be logged"
+        arg_type = String
+    "--bspockID", "-b"
+        help = "the job ID that the spock respawner is keeping track of for iterations of this process"
+        arg_type = Int64
+    "--onSpock", "-s"
+        help = "flag used to indicate we're running on spock"
+        action = :store_true
+    "--respawn", "-r"
+        help = "we should respawn, using the logfile for initial conditions. "*
+            "Otherwise start up from scratch."
+        action = :store_true
+    "--hostname"
+        help = "string used within farm and report filenames"
+        arg_type = String
+        default = String(chomp(read(`hostname`, String)))
+    "my_run_number"
+        help = "if not on spock, will be first positional argument"
+        arg_type = Int64
+        default = 1
+    "tot_n_runs"
+        help = "if not on spock, will be second positional argument"
+        arg_type = Int64
+        default = 1
 end
-if length(ARGS)>1  &&  tryparse(Int64, ARGS[2]) != nothing
-   tot_n_runs    = parse(Int64, ARGS[2]);
-else
-   tot_n_runs = 1;   # I'm being run as part of tot_n_run processes
+parsed_args = parse_args(myargs, s)
+for k in keys(parsed_args)
+    eval(Meta.parse("$k = parsed_args[\"$k\"]"))
+end
+if onSpock
+    hostname = "spock"
 end
 
 
 farmdir = "../../Farms_6Node"
 if !isdir(farmdir); mkdir(farmdir); end
-fbasename = "$farmdir/farm_6Node_$(chomp(read(`hostname`, String)))_"
+fbasename = "$farmdir/farm_6Node_$(hostname)_"
 
 reports_dir = "../../Reports"
 if !isdir(reports_dir); mkdir(reports_dir); end
-report_file = "$reports_dir/r6_$(chomp(read(`hostname`, String)))_$my_run_number"
-if isfile(report_file); rm(report_file); end
+if report_file == nothing
+    if onSpock
+        report_file = "$reports_dir/r6_$(hostname)_$bspockID"
+    else
+        report_file = "$reports_dir/r6_$(hostname)_$my_run_number"
+    end
+end
 
-extra_pars[:seedrand]                  = Int64(my_run_number*round(time()*1000))
 extra_pars[:maxiter]                   = 1000
 extra_pars[:testruns]                  = 10000
 extra_pars[:few_trials]                = 50
@@ -38,53 +67,87 @@ extra_pars[:binarized_delta_threshold] = 0.1    # average frac correct must be w
 extra_pars[:anti_perf_delta]           = 0.05   # delay anti must be at least this worse off than control or choice anti
 extra_pars[:pro_better_than_anti]      = true   # if true, in each condition pro hits must be > anti hits
 
-append_to_file(report_file, "\n\n\nStarting with random seed $(extra_pars[:seedrand])\n\n\n")
-
 just_testing = false; if just_testing   #
     extra_pars[:maxiter]                   = 25
     extra_pars[:testruns]                  = 3
     extra_pars[:few_trials]                = 10
     extra_pars[:many_trials]               = 20
     extra_pars[:binarized_delta_threshold] = 1     # average frac correct must be within this of target
-    extra_pars[:anti_perf_delta]           = 0       # delay anti must be at least this worse off than control or choice anti
+    extra_pars[:anti_perf_delta]           = 0     # delay anti must be at least this worse off than control or choice anti
     extra_pars[:pro_better_than_anti]      = false   # if true, in each condition pro hits must be > anti hits
 end
 
+second_stage_training = false
+if !respawn
+    if isfile(report_file); rm(report_file); end
+
+    extra_pars[:seedrand]                  = Int64(my_run_number*round(time()*1000))
+    append_to_file(report_file, "\n\n\nStarting with random seed $(extra_pars[:seedrand])\n\n\n")
+    start_eta = 0.01
+else
+    fp = open(report_file, "r")
+    s = read(fp, String)
+    close(fp)
+    extra_pars[:seedrand] = parseLast(s, "random seed ", Int64)
+    start_eta             = parseLast(s, " eta=", Float64)
+    seed                  = parseLast(s, " ps=", Vector{Float64})
+
+    tf = findlast("training further", s)
+    if tf != nothing
+        if tf[1] > findlast("random seed", s)[1]
+            second_stage_training = true
+        end
+    end
+end
+
+# --- define the args string vector and bbox for seacrh space bounding box
+args = []; bbox = Dict()
+for k in keys(search_conditions)
+    search_box = search_conditions[k][2]
+    global args = [args; String(k)]
+    # --- search within the full indicated search box
+    global bbox = merge(bbox, Dict(k => Array{Float64}(search_conditions[k][3])))
+end
+args = Array{String}(args)
 
 while true
+    global second_stage_training, respawn, report_file, bspockID, onSpock
+    global hostname, seed
 
-    append_to_file(report_file, "\n\n--- new run -- $(Dates.format(now(), "e, dd u yyyy HH:MM:SS")) ---\n\n")
+    if !second_stage_training
+        if !respawn
+            append_to_file(report_file, "\n\n--- new run -- "*
+                "$(Dates.format(now(), "e, dd u yyyy HH:MM:SS")) ---\n\n")
 
-    args = []; seed = []; bbox = Dict()
-    for k in keys(search_conditions)
-        search_box = search_conditions[k][2]
-        args = [args; String(k)]
-        # --- search within the full indicated search box
-        seed = [seed ; rand()*diff(search_box) .+ search_box[1]]
-        bbox = merge(bbox, Dict(k => Array{Float64}(search_conditions[k][3])))
-    end
-    args = Array{String}(args)
-    seed = Array{Float64}(seed)
+            # Choose a random starting point:
+            seed = [];
+            for k in keys(search_conditions)
+                # --- search within the full indicated search box
+                search_box = search_conditions[k][2]
+                seed = [seed ; rand()*diff(search_box) .+ search_box[1]]
+            end
+            seed = Array{Float64}(seed)
+        end
 
-    extra_pars[:nPro]     = extra_pars[:few_trials]
-    extra_pars[:nAnti]    = extra_pars[:few_trials]
+        extra_pars[:nPro]     = extra_pars[:few_trials]
+        extra_pars[:nAnti]    = extra_pars[:few_trials]
 
-    # Make sure to keep the noise frozen over the search, meaning JJ() needs the seedrand parameter
-    func_quiet =  (;params...) -> JJ(extra_pars[:nPro], extra_pars[:nAnti]; verbose=false,
-        merge(merge(mypars, extra_pars), Dict(params))...)[1]
+        # Make sure to keep the noise frozen over the search, meaning JJ() needs the seedrand parameter
+        func_quiet =  (;params...) -> JJ(extra_pars[:nPro], extra_pars[:nAnti]; verbose=false,
+            merge(merge(mypars, extra_pars), Dict(params))...)[1]
 
-    # try
+        # try
         parsA, trajA, costA, cpm_trajA, ftrajA = bbox_Hessian_keyword_minimization(seed,
             args, bbox, func_quiet,
-            start_eta = 0.01, tol=1e-12, verbose_file=report_file, verbose_timestamp=true,
+            start_eta = start_eta, tol=1e-12, verbose_file=report_file, verbose_timestamp=true,
             verbose=true, verbose_every=10, maxiter=extra_pars[:maxiter])
 
         # evaluate the result with many trials, for accuracy
-        costA, cost1sA, cost2sA, hPA, hAA, dPA, dAA, hBPA, hBAA = JJ(extra_pars[:testruns], extra_pars[:testruns];
-                                                   verbose=false,
-                                                   make_dict(args, parsA, merge(merge(mypars, extra_pars)))...)
+        costA, cost1sA, cost2sA, hPA, hAA, dPA, dAA, hBPA, hBAA =
+            JJ(extra_pars[:testruns], extra_pars[:testruns]; verbose=false,
+                make_dict(args, parsA, merge(merge(mypars, extra_pars)))...)
 
-    ## fix all variables, is mean the best, or should I expand the test so each duration works?
+        ## fix all variables, is mean the best, or should I expand the test so each duration works?
         if size(hBPA,2) > 1
             println(report_file, hBPA);
             println(report_file, hBAA);
@@ -92,14 +155,16 @@ while true
             hBAA = mean(hBAA, dims=2);
         end
 
-        append_to_file(report_file, "mean diff = $(mean(abs.(extra_pars[:opto_targets] - [hBPA hBAA])))\n")
+        append_to_file(report_file,
+            "mean diff = $(mean(abs.(extra_pars[:opto_targets] - [hBPA hBAA])))\n")
         append_to_file(report_file, "all(hBPA .> hBAA) = $(all(hBPA .> hBAA))\n")
         println(report_file, hBPA); println(report_file, hBAA)
         append_to_file(report_file,
-            "\nhBAA[2] <= hBAA[1] - extra_pars[:anti_perf_delta] = $(hBAA[2] <= hBAA[1] - extra_pars[:anti_perf_delta])\n")
+            "\nhBAA[2] <= hBAA[1] - extra_pars[:anti_perf_delta] "*
+            "= $(hBAA[2] <= hBAA[1] - extra_pars[:anti_perf_delta])\n")
         append_to_file(report_file,
-            "hBAA[2] <= hBAA[3] - extra_pars[:anti_perf_delta] = $(hBAA[2] <= hBAA[3] - extra_pars[:anti_perf_delta])\n")
-
+            "hBAA[2] <= hBAA[3] - extra_pars[:anti_perf_delta] = "*
+            "$(hBAA[2] <= hBAA[3] - extra_pars[:anti_perf_delta])\n")
 
         if mean(abs.(extra_pars[:opto_targets] - [hBPA hBAA])) < extra_pars[:binarized_delta_threshold]  &&
             (!extra_pars[:pro_better_than_anti] || all(hBPA .> hBAA))  &&
@@ -111,23 +176,28 @@ while true
             append_to_file(report_file, "\n\n**** training further **** " *
                 "$(Dates.format(now(), "e, dd u yyyy HH:MM:SS")) ---\n\n")
 
-            extra_pars[:nPro]     = extra_pars[:many_trials]
-            extra_pars[:nAnti]    = extra_pars[:many_trials]
+            second_stage_training = true
+            seed = parsA
+        end
+    end
 
-            func_chatty =  (;params...) -> JJ(extra_pars[:nPro], extra_pars[:nAnti]; verbose=true,
+    if second_stage_training
+        extra_pars[:nPro]     = extra_pars[:many_trials]
+        extra_pars[:nAnti]    = extra_pars[:many_trials]
+
+        func_chatty =  (;params...) -> JJ(extra_pars[:nPro], extra_pars[:nAnti]; verbose=true,
                                               verbose_file = report_file,
                                               merge(merge(mypars, extra_pars), Dict(params))...)[1]
             try
-
-                pars3, traj3, cost3, cpm_traj3, ftraj3 = bbox_Hessian_keyword_minimization(parsA,
-                                                                 args, bbox, func_chatty,  verbose_timestamp=true,
-                                                                 start_eta = 0.01, tol=1e-12, verbose_file=report_file,
-                                                                 verbose=true, verbose_every=10, maxiter=extra_pars[:maxiter])
+                pars3, traj3, cost3, cpm_traj3, ftraj3 = bbox_Hessian_keyword_minimization(seed,
+                     args, bbox, func_chatty,  verbose_timestamp=true,
+                     start_eta = start_eta, tol=1e-12, verbose_file=report_file,
+                     verbose=true, verbose_every=10, maxiter=extra_pars[:maxiter])
 
                 # evaluate the result with many trials, for accuracy
-                cost, cost1s, cost2s, hP, hA, dP, dA, hBP, hBA = JJ(extra_pars[:testruns], extra_pars[:testruns];
-                                                               verbose=false,
-                                                               make_dict(args, pars3, merge(merge(mypars, extra_pars)))...)
+                cost, cost1s, cost2s, hP, hA, dP, dA, hBP, hBA =
+                    JJ(extra_pars[:testruns], extra_pars[:testruns]; verbose=false,
+                        make_dict(args, pars3, merge(merge(mypars, extra_pars)))...)
 
                 myfilename = next_file(fbasename, 4)
                 myfilename = myfilename*".jld"
@@ -167,4 +237,7 @@ while true
 
     extra_pars[:seedrand] = extra_pars[:seedrand]+1
     append_to_file(report_file, "\n\nChanging to random seed $(extra_pars[:seedrand])\n\n\n")
+
+    second_stage_training = false
+    respawn = false
 end
