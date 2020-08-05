98a4551a47d65a1cf65bc8ff34a9e6bcea39112c
Carlos Brody <brody@spock-login.pni.Princeton.EDU>
2020-08-05 16:36:49 -0400

    added capability so remaining args are passed to the Julia script, and a taskIDOffset that is added to the run number

--- A_REWRITE/spockFarm.sh
+++ A_REWRITE/spockFarm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #SBATCH -J 'spockFarm'
-#SBATCH -o  log-spockFarm-%j.out
+#SBATCH -o  log2-spockFarm-%j.out
 #SBATCH -p Brody
 #SBATCH --time 96:00:00
 #SBATCH -c 1
@@ -13,8 +13,13 @@ module load julia/1.2.0
 echo "Slurm Job ID, unique: $SLURM_JOB_ID"
 echo "Slurm Array Task ID, relative: $SLURM_ARRAY_TASK_ID"
 
+juliaScript=$1
+shift
+taskIDOffset=175
+
+
 # call the julia script which will start the farm
-julia $1 --bspockID ${SLURM_ARRAY_JOB_ID}_$SLURM_ARRAY_TASK_ID --sleepFor 10 --onSpock --hostname spock
+julia $juliaScript --bspockID ${SLURM_ARRAY_JOB_ID}_$SLURM_ARRAY_TASK_ID --myRunNumber $((SLURM_ARRAY_TASK_ID + taskIDOffset)) --onSpock --hostname spock $@
 
 # move the log file to an input-dependent location
 # mv log-array-example-${SLURM_JOB_ID}.out $1/log-array-example-${SLURM_JOB_ID}.out
