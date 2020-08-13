9486b18c84a086d3418a369c96d81d645f8b777e
Carlos Brody <carlosbrody@gmail.com>
2020-08-12 23:27:27 -0400

    Changed to pull reports from spock

--- A_REWRITE/pull_reports.sh
+++ A_REWRITE/pull_reports.sh
@@ -1,10 +1,22 @@
 #!/bin/csh
 
-echo gcloud compute ssh --command "\rm -rf Reportz.tz ; tar cvfz Reports.tz Reports" carlosbrody@$argv[1]
-gcloud compute ssh --command "\rm -rf Reportz.tz ; tar cvfz Reports.tz Reports" carlosbrody@$argv[1]
+echo ssh brody@spock.princeton.edu "\rm -rf Reportz.tz ; tar cvfz Reports.tz Reports"
+ssh brody@spock.princeton.edu "\rm -rf Reportz.tz ; tar cvfz Reports.tz Reports"
 
-echo "gcloud compute scp carlosbrody@${argv[1]}:Reports.tz ../../Reports.tz"
-gcloud compute scp carlosbrody@${argv[1]}:Reports.tz ../../Reports.tz
+echo "scp brody@spock.princeton.edu:Reports.tz ../../Reports.tz"
+scp brody@spock.princeton.edu:Reports.tz ../../Reports.tz
 
 echo "pushd ../.. ; tar xvfz Reports.tz ; popd"
 pushd ../.. ; tar xvfz Reports.tz ; popd
+
+
+# ----------
+
+# echo gcloud compute ssh --command "\rm -rf Reportz.tz ; tar cvfz Reports.tz Reports" carlosbrody@$argv[1]
+# gcloud compute ssh --command "\rm -rf Reportz.tz ; tar cvfz Reports.tz Reports" carlosbrody@$argv[1]
+#
+# echo "gcloud compute scp carlosbrody@${argv[1]}:Reports.tz ../../Reports.tz"
+# gcloud compute scp carlosbrody@${argv[1]}:Reports.tz ../../Reports.tz
+#
+# echo "pushd ../.. ; tar xvfz Reports.tz ; popd"
+# pushd ../.. ; tar xvfz Reports.tz ; popd
