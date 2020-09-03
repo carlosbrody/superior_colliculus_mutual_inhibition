0faa95e483961c7ca3b9594f9eca70078192129f
Alex Piet <alexpiet@gmail.com>
2020-09-03 14:26:03 -0700

    updates in dprime analysis

--- A_REWRITE/dprime_analysis.jl
+++ A_REWRITE/dprime_analysis.jl
@@ -24,16 +24,33 @@ function save_solutions(;num_trials=10)
 end
 
 function plot_dprime(;timestep=55,opto_condition=1,time_condition=1)
+    
+    # Grab the dprime values
     pro_pa_d, ant1_pa_d, ant2_pa_d, pro_choice_d, ant1_choice_d, ant2_choice_d = get_dprime(timestep=timestep, opto_condition=opto_condition, time_condition=time_condition)
+
+    # Make a histogram of just choice encoding
     figure(figsize=(5,4))     
-    plt[:hist](pro_choice_d, bins=10, color="black", alpha=0.3, label="Pro")
-    #plt[:hist](vcat(ant1_choice_d,ant2_choice_d), bins=10, color="r", alpha=0.3, label="Anti")
-    plt[:hist](ant1_choice_d, bins=10, color="r", alpha=0.3, label="Anti-1")
-    plt[:hist](ant2_choice_d, bins=10, color="m", alpha=0.3, label="Anti-2")
+    plt[:hist](pro_choice_d, bins=10, color="k", alpha=.2, label="Pro")
+    plt[:hist](ant1_choice_d, bins=10, color="r", alpha=0.2, label="Anti-1")
+    plt[:hist](ant2_choice_d, bins=10, color="m", alpha=0.2, label="Anti-2")
+    axvline(0, color="k", linestyle="--")
+    plt[:xlabel]("Choice d'")
+    plt[:ylabel]("Count")
+    plt[:title]("Timestep: "*string(timestep))
+    plt[:savefig]("choice_dist_timestep_"*string(timestep)*".png")
+    
+    # Make a histogram of just rule encoding
+    figure(figsize=(5,4))     
+    plt[:hist](pro_pa_d, bins=10, color="k", alpha=.2, label="Pro")
+    plt[:hist](ant1_pa_d, bins=10, color="r", alpha=0.2, label="Anti-1")
+    plt[:hist](ant2_pa_d, bins=10, color="m", alpha=0.2, label="Anti-2")
     axvline(0, color="k", linestyle="--")
     plt[:xlabel]("Pro/Anti d'")
     plt[:ylabel]("Count")
+    plt[:title]("Timestep: "*string(timestep))
+    plt[:savefig]("proanti_dist_timestep_"*string(timestep)*".png")
 
+    # Make the scatter plot figure
     figure(figsize=(5,4))
     plot(pro_pa_d, pro_choice_d, "ko",label="Pro")
     plot(ant1_pa_d, ant1_choice_d, "ro",label="Anti-1")
@@ -42,7 +59,8 @@ function plot_dprime(;timestep=55,opto_condition=1,time_condition=1)
     axhline(0, color="k", linestyle="--")
     plt[:xlabel]("Pro/Anti d'")
     plt[:ylabel]("Choice d'")
-
+    plt[:title]("Timestep: "*string(timestep))
+    plt[:savefig]("scatter_timestep_"*string(timestep)*".png")
 end
 
 function get_dprime(; timestep=55,opto_condition=1,time_condition=1)
@@ -120,30 +138,30 @@ function compute_choice_dprime(pro_traces, anti_traces,d; timestep=55)
     ant_trials_ipsi_choice = anti_traces[d["ProNodeID"],end,:][1,:] .> anti_traces[d["ProNodeID"],end,:][2,:]   
 
     # Merge pro units across pro/anti trials into ipsi/contra choice   
-    pro_trials_pro_units_ipsi_choice = vcat(pro_trials_pro_units[1,pro_trials_ipsi_choice],pro_trials_pro_units[2,pro_trials_ipsi_choice])
-    ant_trials_pro_units_ipsi_choice = vcat(ant_trials_pro_units[1,ant_trials_ipsi_choice],ant_trials_pro_units[2,ant_trials_ipsi_choice])
+    pro_trials_pro_units_ipsi_choice = vcat(pro_trials_pro_units[1,pro_trials_ipsi_choice],pro_trials_pro_units[2,.~pro_trials_ipsi_choice])
+    ant_trials_pro_units_ipsi_choice = vcat(ant_trials_pro_units[1,ant_trials_ipsi_choice],ant_trials_pro_units[2,.~ant_trials_ipsi_choice])
     pro_units_ipsi_choice = vcat(pro_trials_pro_units_ipsi_choice,ant_trials_pro_units_ipsi_choice )
 
-    pro_trials_pro_units_cont_choice = vcat(pro_trials_pro_units[1,.~pro_trials_ipsi_choice],pro_trials_pro_units[2,.~pro_trials_ipsi_choice])
-    ant_trials_pro_units_cont_choice = vcat(ant_trials_pro_units[1,.~ant_trials_ipsi_choice],ant_trials_pro_units[2,.~ant_trials_ipsi_choice])
+    pro_trials_pro_units_cont_choice = vcat(pro_trials_pro_units[1,.~pro_trials_ipsi_choice],pro_trials_pro_units[2,pro_trials_ipsi_choice])
+    ant_trials_pro_units_cont_choice = vcat(ant_trials_pro_units[1,.~ant_trials_ipsi_choice],ant_trials_pro_units[2,ant_trials_ipsi_choice])
     pro_units_cont_choice = vcat(pro_trials_pro_units_cont_choice,ant_trials_pro_units_cont_choice )
 
     # Merge anti units across pro/anti trials into ipsi/contra choice 
-    pro_trials_ant1_units_ipsi_choice = vcat(pro_trials_ant_units[1,pro_trials_ipsi_choice],pro_trials_ant_units[4,pro_trials_ipsi_choice])
-    ant_trials_ant1_units_ipsi_choice = vcat(ant_trials_ant_units[1,ant_trials_ipsi_choice],ant_trials_ant_units[4,ant_trials_ipsi_choice])
+    pro_trials_ant1_units_ipsi_choice = vcat(pro_trials_ant_units[1,pro_trials_ipsi_choice],pro_trials_ant_units[4,.~pro_trials_ipsi_choice])
+    ant_trials_ant1_units_ipsi_choice = vcat(ant_trials_ant_units[1,ant_trials_ipsi_choice],ant_trials_ant_units[4,.~ant_trials_ipsi_choice])
     ant1_units_ipsi_choice = vcat(pro_trials_ant1_units_ipsi_choice,ant_trials_ant1_units_ipsi_choice )
 
-    pro_trials_ant1_units_cont_choice = vcat(pro_trials_ant_units[1,.~pro_trials_ipsi_choice],pro_trials_ant_units[4,.~pro_trials_ipsi_choice])
-    ant_trials_ant1_units_cont_choice = vcat(ant_trials_ant_units[1,.~ant_trials_ipsi_choice],ant_trials_ant_units[4,.~ant_trials_ipsi_choice])
+    pro_trials_ant1_units_cont_choice = vcat(pro_trials_ant_units[1,.~pro_trials_ipsi_choice],pro_trials_ant_units[4,pro_trials_ipsi_choice])
+    ant_trials_ant1_units_cont_choice = vcat(ant_trials_ant_units[1,.~ant_trials_ipsi_choice],ant_trials_ant_units[4,ant_trials_ipsi_choice])
     ant1_units_cont_choice = vcat(pro_trials_ant1_units_cont_choice,ant_trials_ant1_units_cont_choice )
 
     # Merge anti units across pro/anti trials into ipsi/contra choice 
-    pro_trials_ant2_units_ipsi_choice = vcat(pro_trials_ant_units[2,pro_trials_ipsi_choice],pro_trials_ant_units[3,pro_trials_ipsi_choice])
-    ant_trials_ant2_units_ipsi_choice = vcat(ant_trials_ant_units[2,ant_trials_ipsi_choice],ant_trials_ant_units[3,ant_trials_ipsi_choice])
+    pro_trials_ant2_units_ipsi_choice = vcat(pro_trials_ant_units[2,pro_trials_ipsi_choice],pro_trials_ant_units[3,.~pro_trials_ipsi_choice])
+    ant_trials_ant2_units_ipsi_choice = vcat(ant_trials_ant_units[2,ant_trials_ipsi_choice],ant_trials_ant_units[3,.~ant_trials_ipsi_choice])
     ant2_units_ipsi_choice = vcat(pro_trials_ant2_units_ipsi_choice,ant_trials_ant2_units_ipsi_choice )
 
-    pro_trials_ant2_units_cont_choice = vcat(pro_trials_ant_units[2,.~pro_trials_ipsi_choice],pro_trials_ant_units[3,.~pro_trials_ipsi_choice])
-    ant_trials_ant2_units_cont_choice = vcat(ant_trials_ant_units[2,.~ant_trials_ipsi_choice],ant_trials_ant_units[3,.~ant_trials_ipsi_choice])
+    pro_trials_ant2_units_cont_choice = vcat(pro_trials_ant_units[2,.~pro_trials_ipsi_choice],pro_trials_ant_units[3,pro_trials_ipsi_choice])
+    ant_trials_ant2_units_cont_choice = vcat(ant_trials_ant_units[2,.~ant_trials_ipsi_choice],ant_trials_ant_units[3,ant_trials_ipsi_choice])
     ant2_units_cont_choice = vcat(pro_trials_ant2_units_cont_choice,ant_trials_ant2_units_cont_choice )
 
     # Compute dprime
