ee85e1142b73985f8cf20752aae21838830d1acd
Alex Piet <alexpiet@gmail.com>
2021-03-30 22:52:00 -0700

    part of figure 6 script

---
+++ Nature_Neuro_Figures/Fig_6.jl
@@ -0,0 +1,22 @@
+# This script makes Figure 6
+# This script works in Julia 0.6.4
+
+# load relevant files
+include("results_analysis.jl")
+include("svd_cluster.jl")
+include("parameter_analysis.jl")
+
+# load (arbitrary first farm for list of parameter names "args")
+f1 = load("MiniC32/farm_C32_Farms_C32_spock-brody01-01_0001.jld")
+args = f1["args"];
+
+# load results of entire farm run
+results = load_farm_cost_filter("C32", "MiniC32"; threshold = -0.0001)
+
+# Panel C, Example solution
+plot_farm_figure("MiniC32/farm_C32_Farms_C32_spock-brody01-02_0098.jld");
+
+# Panel B
+# Panel D
+
+
