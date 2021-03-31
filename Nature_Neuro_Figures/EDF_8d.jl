863f98f7cca0114d06a713d5337ed8172ec494ec
Alex Piet <alexpiet@gmail.com>
2021-03-30 23:35:28 -0700

    updating edf 8d

---
+++ Nature_Neuro_Figures/EDF_8d.jl
@@ -0,0 +1,4 @@
+# This is just a stub to point you to the script for the schur analysis
+# everything is done in this file
+# Just running this line will print all the results
+include("network_analysis.jl")
